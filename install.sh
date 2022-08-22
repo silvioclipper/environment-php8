43ce3c894080b662065ff0a3170b3f9675767669
silvioclipper <silvioclipper@gmail.com>
2022-08-22 00:48:01 -0300

    choir:create installsh for install composer php

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+apt-get update -y
+php -r "copy('http://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer   
+php -S  0.0.0.0:80
+# tail -f /dev/null
