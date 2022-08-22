37accb633f4b4137efe230533a49e2db8d4fb6e8
silvadias <luisdias04@hotmail.com>
2022-08-22 20:13:23 -0300

    edit teste

--- index.php
+++ index.php
@@ -3,16 +3,31 @@
 echo "'Hey, I, oh, I'm still alive.'";
 
 $dsn = 'mysql:
-        host=localhost;
-        dbname=php_server_test';        
-$user = 'user';
-$password = 'userpassword';
+        host=db;
+        dbname=php_server_test
+        port=3307';        
+$user = 'root';
+$password = 'root';
 
-try {
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if($dbh){echo 'db connected';}
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+//try {
+ //   $dbh = new PDO($dsn, $user, $password);
+ //   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+//} catch (PDOException $e) {
+ //   echo 'Connection failed: ' . $e->getMessage();
+//}
+
+$link = mysql_connect('localhost', 'root', 'root');
+
+if (!$link) {
+    die('Não foi possível conectar: ' . mysql_error());
 }
+echo 'Conexão bem sucedida';
+$db_list = mysql_list_dbs($link);
+
+while ($row = mysql_fetch_object($db_list)) {
+     echo $row->Database . "\n";
+}
+
+
 echo phpinfo();
