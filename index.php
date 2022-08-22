a53fe948cade4bbed9024d43a50c6bfa1c9e40b4
silvadias <luisdias04@hotmail.com>
2022-08-22 13:33:15 -0300

    pdo connection code included in line

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
 echo "'Hey, I, oh, I'm still alive.'";
+
+$dsn = 'mysql:
+        host=localhost;
+        dbname=php_server_test';        
+$user = 'user';
+$password = 'userpassword';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if($dbh){echo 'db connected';}
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+echo phpinfo();
