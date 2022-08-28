b44f9e66f17c126bc9f2db667046c9b3e6e54c45
silvioclipper <silvioclipper@gmail.com>
2022-08-28 16:53:55 -0300

    chor: modify load inder folder /public

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 apt-get update -y
+apt install docker-php-ext-install mysqli -y && docker-php-ext-enable mysqli 
 php -r "copy('http://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 mv composer.phar /usr/local/bin/composer   
-php -S  0.0.0.0:80
+php -S  0.0.0.0:80 -t public
