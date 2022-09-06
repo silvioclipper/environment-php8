088721c03bcf6ec1b742a9020fe7504c528eb684
silvioclipper <silvioclipper@gmail.com>
2022-09-06 19:16:33 -0300

    chore:update install.sh include install composer.phar

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 apt-get update -y
-apt install docker-php-ext-install mysqli -y && docker-php-ext-enable mysqli 
+apt install docker-php-ext-install mysqli zip unzip -y && docker-php-ext-enable mysqli 
+
 php -r "copy('http://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer   
+mv composer.phar /usr/local/bin/composer
+composer require --dev phpunit/phpunit ^9.5
+# composer.phar dump-autoload --no-scripts --optimize
 php -S  0.0.0.0:80 -t public
